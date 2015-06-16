f1f8e6959124a993d3c9639e910381a36e5bfb52
Gabriel <developer.gabriel@gmail.com>
2015-06-16 17:01:06 -0300

    Incluindo método pra testar funcionalidade do carrinho

--- Loja.Tests/TestCarrinhoController.cs
+++ Loja.Tests/TestCarrinhoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Loja.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +8,17 @@ using System.Threading.Tasks;
 
 namespace Loja.Tests
 {
+    [TestClass]
     public class TestCarrinhoController
     {
-        
+        [TestMethod]
+        public void TestaQueAConexaoComOServidorFunciona()
+        {
+            var controller = new CarrinhoController();
+
+            var resultado = controller.Busca();
+
+            Assert.IsNotNull(resultado);
+        }
     }
 }
