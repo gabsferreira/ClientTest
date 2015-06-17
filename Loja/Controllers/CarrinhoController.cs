1ba6341b39dad615b57f8ac62a75a106e18287ea
Gabriel <developer.gabriel@gmail.com>
2015-06-17 15:01:31 -0300

    FIM DO CAPITULO 5 - Fazendo POST com o método Adiciona pra criar um novo carrinho

--- Loja/Controllers/CarrinhoController.cs
+++ Loja/Controllers/CarrinhoController.cs
@@ -2,10 +2,13 @@
 using Loja.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Loja.Controllers
 {
@@ -19,8 +22,18 @@ namespace Loja.Controllers
         }
 
         [HttpPost]
-        public string Adiciona()
+        public string Adiciona([FromBody]string conteudo)
         {
+            StringReader StrReader = new StringReader(conteudo);
+            XmlSerializer serializer = new XmlSerializer(typeof(Carrinho));
+            XmlTextReader XmlReader = new XmlTextReader(StrReader);
+            
+            Carrinho objCarrinho = (Carrinho)serializer.Deserialize(XmlReader);
+            XmlReader.Close();
+            StrReader.Close();
+
+            new CarrinhoDAO().Adiciona(objCarrinho);
+            
             return "<status>sucesso</status";
         }
     }
