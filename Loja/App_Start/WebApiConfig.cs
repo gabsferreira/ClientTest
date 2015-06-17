52b0a57e3e0a6832480a304dc1d8747cbf789252
Gabriel <developer.gabriel@gmail.com>
2015-06-17 16:58:27 -0300

    Capitulo 10 - adicionando logging na aplicação pra ver o que tá pegando do lado do cliente.

--- Loja/App_Start/WebApiConfig.cs
+++ Loja/App_Start/WebApiConfig.cs
@@ -10,6 +10,7 @@ namespace Loja
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableSystemDiagnosticsTracing();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
